04e4dcf40ea4025dad942e40061a95f9a2eb2eab
7goldfinger7 <102880876+7goldfinger7@users.noreply.github.com>
2022-04-28 20:10:15 -0400

    Add files via upload

---
+++ Registro.php
@@ -0,0 +1,27 @@
+<?php
+include('conexion.php');
+
+session_start();
+if(isset($_SESSION['nombredelusuario']))
+{
+	header('location: Lista.php');
+}
+
+if(isset($_POST["btnregistrar"]))
+{
+
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+
+	$sqlgrabar = "INSERT INTO login(usuario,password) values ('$nombre','$pass')";
+	
+	if(mysqli_query($conn,$sqlgrabar))
+	{
+		echo "<script> alert('Usuario registrado con exito: $nombre'); window.location='Registro.php' </script>";
+	}else 
+	{
+		echo "Error: ".$sql."<br>".mysql_error($conn);
+	}
+} 
+?>
