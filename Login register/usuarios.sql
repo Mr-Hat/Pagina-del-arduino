76abae4c20a2e274e04205f05c4f45fd4109e4bc
7goldfinger7 <102880876+7goldfinger7@users.noreply.github.com>
2022-07-17 19:45:28 -0400

    Delete Login register directory

--- Login register/usuarios.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO usuarios(contraseña,nombre_usuario,nombre,apellido_paterno,apellido_materno,correo) VALUES ("a4843$#a","Noobmaster696969","pepito","perez","perez","pepito.perez@gamar.cl");
