4d5828386fc949a829fcb0ae4a3e683a8e9d6965
7goldfinger7 <102880876+7goldfinger7@users.noreply.github.com>
2022-07-27 18:54:31 -0400

    Update db.sql

--- Gestion del hogar 2.0/db.sql
+++ Gestion del hogar 2.0/db.sql
@@ -28,13 +28,15 @@ CREATE TABLE mascotas(
 );
 
 CREATE TABLE usuarios(
+  id int NOT NULL AUTO_INCREMENT,
   contraseña varchar(20),
   nombre_usuario varchar(20),
   nombre varchar(100),
   apellido_paterno varchar(100),
   apellido_materno varchar(100),
   correo varchar(100),
-  PRIMARY KEY (contraseña)
+  PRIMARY KEY (nombre_usuario),
+  PRIMARY KEY (id)
 );
 
 
