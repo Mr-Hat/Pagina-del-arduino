5c03c2f0b31d535d476d750386ce54bf429cd9d2
7goldfinger7 <102880876+7goldfinger7@users.noreply.github.com>
2022-07-28 12:08:50 -0400

    Update conexion_general.php

--- Gestion del hogar 2.0/conexion_general.php
+++ Gestion del hogar 2.0/conexion_general.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "g1234"; //cabiar dependiendo del pc 
+$password = ""; //cabiar dependiendo del pc 
 $dbname = "Gestion";
     //crear la coexion
 $conn = mysqli_connect($servername, $username, $password, $dbname);
